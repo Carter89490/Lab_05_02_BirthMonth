fa7b9ce7fe4f423fd4b9a3b1953cb8c9cee1270a
Carter Holsinger <113468540+Carter89490@users.noreply.github.com>
2022-10-04 14:28:27 -0400

    Add files via upload

---
+++ Lab_05_02_BirthMonth/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int brithMon = 13;
+            if(brithMon>=1 && brithMon <= 12)
+                System.out.println("Your brith month is " + brithMon);
+            else
+                System.out.println("You entered an invalid month");
+
+    }
+}
